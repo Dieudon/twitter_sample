1f5770e245c93e0414bb46d00ba458410c5142d5
Dieudon <d.egstorf@publitas.com>
2013-08-22 15:00:20 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest, :password, :password_confirmation, :authenticate
-  before_save { email.downcase! }
+  
   validates :name, presence: true, length: { maximum: 50 }
+  
+  before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
+  
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,14 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('h1', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+    
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
@@ -38,6 +46,15 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('h1', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
