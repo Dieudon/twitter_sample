a28eb79f230e1d6db7faa621f86378a6f66e19e4
Dieudon <d.egstorf@publitas.com>
2013-08-29 11:23:41 +0200

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,6 +23,11 @@ describe "Authentication" do
       it { should have_title(full_title(page_title)) }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
       
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out', href: signout_path) }
+      it { should have_link('Sign in', href: signin_path) }
+      
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
@@ -33,11 +38,7 @@ describe "Authentication" do
     
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_selector('h1', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
@@ -45,26 +46,17 @@ describe "Authentication" do
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
+      #describe "visiting the create page" do
+       # before { visit new_user_path(user) }
+        #it { should have_title('| Home') }
+      #end
+      
+      
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
     end
-    
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
-
-      it { should have_title(full_title(user.name)) }
-
-      it { should have_link('Users',    href: users_path) }
-      it { should have_link('Profile',  href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-
-      it { should_not have_link('Sign in', href: signin_path) }
-    end
-    
     
   end
   
@@ -76,17 +68,25 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
+        before { visit edit_user_path(user) }
+        before { sign_in user }
 
         describe "after signing in" do
 
           it "should render the desired protected page" do
             page.should have_title(full_title('Edit user'))
+          end
+          
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              page.should have_title(full_title(user.name))
+            end
           end
         end
       end
@@ -136,8 +136,7 @@ describe "Authentication" do
         before { delete user_path(user) }
         specify { response.should redirect_to(root_url) }
       end
-    end
-    
+    end    
   end
   
   
